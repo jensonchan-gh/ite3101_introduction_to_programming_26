b3d28411d56e6a454c4fb8b09c5e06d7709c10cc
Jenson Chan <90683153+jensonchan-gh@users.noreply.github.com>
2026-09-22 02:43:21 +0000

    [skip actions] lab/lab04/ch04_t02_compare_closely.py

--- lab/lab04/ch04_t02_compare_closely.py
+++ lab/lab04/ch04_t02_compare_closely.py
@@ -4,10 +4,10 @@
 bool_one = True  # We did this one for you!
 
 # Set this to True if 100 == (2 * 50) or to False otherwise.
-bool_two = False
+bool_two = True
 
 # Set this to True if 19 <= 19 or to False if it is not.
-bool_three = False
+bool_three = True
 
 # Set this to True if -22 >= -18 or to False if it is not.
 bool_four = True
